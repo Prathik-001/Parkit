5077f8d2d282d12d0025c59f32eef391183b5991
Prathik <prathikhegdecoc@gmail.com>
2024-11-14 14:34:39 +0530

    rechanged

--- src/components/Login/login.jsx
+++ src/components/Login/login.jsx
@@ -2,47 +2,43 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function login() {
+function LoginPage() {
+
+
     return (
         <div className="login-form">
             <div className='h1'>
-            <h1>Login</h1>
+                <h1>Login</h1>
             </div>
             <div className='block'>
-            <div className='log-1'>
-            <form>
-            <label For="username">USER-NAME</label>
-                <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    placeholder='User-Name'
-                    required/><br/>
-                <label For="email">EMAIL ID</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    placeholder='Email-Address'
-                    required/><br/>
-                <label For="password">PASSWORD</label>
-                <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    placeholder='Password'
-                    required/>
-                <button type="submit">Login</button>
-                </form>
+                <div className='log-1'>
+                    <form>
+                        <label htmlFor="email">EMAIL ID</label>
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder='Email-Address'
+                            required
+                        /><br/>
+                        <label htmlFor="password">PASSWORD</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder='Password'
+                            required
+                        />
+                        <button type="submit">Login</button>
+                    </form>
                 </div>
                 <div className='log-2'> background image </div>
-                </div>
-                <div className='log-3'>
-                <p>Don't have an account? <Link to="/signup"  className='text-[#5393d7] hover:no-underline hover:text-[#0d2ed2]'>Create New</Link></p>
             </div>
+            <div className='log-3'>
+                <p>Don't have an account? <Link to="/signup" className='text-[#5393d7] hover:no-underline hover:text-[#0d2ed2]'>Create New</Link></p>
             </div>
-
+        </div>
     );
 };
 
-export default login;
+export default LoginPage;
